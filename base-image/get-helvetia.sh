731e600d149b8722b0726b543727bc45cd277493
Mircea Filip Lungu <mircea.lungu@gmail.com>
2013-04-19 16:00:30 +0200

    downloads all the prerequisites, and loads from the local git branch

--- base-image/get-helvetia.sh
+++ base-image/get-helvetia.sh
@@ -3,3 +3,4 @@ wget http://hudson.lukas-renggli.ch/job/Helvetia/lastSuccessfulBuild/artifact/He
 unzip Helvetia-OneClick.zip
 rm -rf Helvetia-OneClick.zip
 
+./Helvetia.app/Contents/MacOS/Squeak\ VM\ Opt ./Helvetia.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st
