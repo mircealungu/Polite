0b19547fb0068750c280841c9e8d6467a743d87d
Mircea Lungu <mircea.lungu@gmail.com>
2013-05-10 16:51:21 +0200

    helper script to start images

--- base-image/start.sh
+++ base-image/start.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
 if [[ $# -ne 1 ]]; then
-	ls Helvetia.app/Contents/MacOS/Squeak\ VM\ Opt  Helvetia.app/Contents/Resources/*.image
+	echo "=== Pass one of the following images as argument ==="
+	ls Helvetia.app/Contents/Resources/*.image | xargs -n1 basename | sed -e "s/.image//g" | sort
 	exit
 fi
 
