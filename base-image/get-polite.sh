30eccbabc7d5728e0b24d2817cdb0dbd7514ba04
Mircea Lungu <mircea.lungu@gmail.com>
2013-09-05 16:07:37 +0200

    now also for Linux!

--- base-image/get-polite.sh
+++ base-image/get-polite.sh
@@ -1,6 +1,14 @@
 #/bin/sh
+
 wget http://hudson.lukas-renggli.ch/job/Helvetia/lastSuccessfulBuild/artifact/Helvetia-OneClick.zip
 unzip Helvetia-OneClick.zip
 rm -rf Helvetia-OneClick.zip
 
-./Helvetia.app/Contents/MacOS/Squeak\ VM\ Opt ./Helvetia.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st
+if [[ `uname -s` == "Linux" ]]; then
+	echo "Linux detected..."
+	./Helvetia.app/Contents/Linux/squeak ./Helvetia.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st 
+else
+	echo "OS X detected..."
+	./Helvetia.app/Contents/MacOS/Squeak\ VM\ Opt ./Helvetia.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st 
+fi
+
