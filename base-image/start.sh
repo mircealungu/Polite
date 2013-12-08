af8af22e4fcf6c3a0692c81b41c1ef05b50eb6e9
Mircea Lungu <mircea.lungu@gmail.com>
2013-12-08 10:31:50 +0100

    rename

--- base-image/start.sh
+++ base-image/start.sh
@@ -2,9 +2,9 @@
 
 if [[ $# -ne 1 ]]; then
 	echo "=== Pass one of the following images as argument ==="
-	ls Helvetia.app/Contents/Resources/*.image | xargs -n1 basename | sed -e "s/.image//g" | sort
+	ls Polite.app/Contents/Resources/*.image | xargs -n1 basename | sed -e "s/.image//g" | sort
 	exit
 fi
 
-Helvetia.app/Contents/MacOS/Squeak\ VM\ Opt  Helvetia.app/Contents/Resources/$1.image
+Polite.app/Contents/MacOS/Squeak\ VM\ Opt  Polite.app/Contents/Resources/$1.image
 
