92f35089c912d750c00db52ae8ee11e9eca0fbe4
Mircea Lungu <mircea.lungu@gmail.com>
2013-12-08 10:23:26 +0100

    renaming to Polite.app

--- base-image/get-polite.sh
+++ base-image/get-polite.sh
@@ -3,12 +3,14 @@
 wget http://hudson.lukas-renggli.ch/job/Helvetia/lastSuccessfulBuild/artifact/Helvetia-OneClick.zip
 unzip Helvetia-OneClick.zip
 rm -rf Helvetia-OneClick.zip
+mv Helvetia.app Polite.app
+cp ../materials/PoliteBowB.icns Polite.app/Contents/Resources/Squeak.icns
 
 if [[ `uname -s` == "Linux" ]]; then
 	echo "Linux detected..."
-	./Helvetia.app/Contents/Linux/squeak ./Helvetia.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st 
+	./Polite.app/Contents/Linux/squeak ./Polite.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st 
 else
 	echo "OS X detected..."
-	./Helvetia.app/Contents/MacOS/Squeak\ VM\ Opt ./Helvetia.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st 
+	./Polite.app/Contents/MacOS/Squeak\ VM\ Opt ./Polite.app/Contents/Resources/Helvetia.image `pwd`/prepareImage.st 
 fi
 
